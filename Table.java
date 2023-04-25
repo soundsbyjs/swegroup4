e19f92b12bd688e0ef07ce1b9a14e48e8f68c140
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2023-04-25 13:41:43 -0400

    Update Table.java

--- Table.java
+++ Table.java
@@ -3,12 +3,21 @@ public class Table {
     protected int tableNum;
     private boolean clean;
     private boolean occupied;
+    private order tableOrder;
   
   //constructor to set table number when added to floor layout
     public Table(int num) {
         tableNum = num;
         clean = true;
         occupied = false;
+    }
+  //adds an order to table
+    public void addOrder(order newOrder){
+        tableOrder = newOrder;
+    }
+  //returns the table order
+    public order getOrder() {
+        return tableOrder;
     }
   //sets the table status to clean
     public void setClean(String item) {
@@ -18,10 +27,11 @@ public class Table {
     public void setOccupied(String item) {
         occupied = true;
     }
-  //sets table status to vacant and dirty when customers leave
+  //sets table status to vacant and dirty and removes order information when customers leave
     public void setVacant(String item) {
         occupied = false;
         clean = false;
+        tableOrder = null;
     }
   //returns table status as either "Occupied", "Clean", or "Dirty"
     public String getTableStatus() {
