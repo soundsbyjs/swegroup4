1cc3d928bb44bdd6b810741a53312012afdfa6cd
Adamlb493 <adamlb493@gmail.com>
2023-04-26 18:30:04 -0400

    Added Files Via Upload

--- Menu.java
+++ Menu.java
@@ -1,69 +1,62 @@
 import java.io.File;
-import java.io.IOException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
     protected ArrayList<List<Item>> menuItems;
-    public Menu(ArrayList<List<Item>> menuList)
+    public Menu()
+    {
+        File menuAppetizers = new File("Menu_Appetizers.txt");
+        File menuSalads = new File("Menu_Salads.txt");
+        File menuEntrees = new File("Menu_Entrees.txt");
+        File menuJFavorites = new File("Menu_JFavorites.txt");
+        File menuBurgers = new File("Menu_Burgers.txt");
+        menuItems.add(scanMenu(menuAppetizers));
+    }
+    public List<Item> scanMenu(File fileName)
     {
         FileReader fr;
         ///creating a Reader object
+        String currentCate = null;
+        String currentName = null;
+        Float currentCost = null;
+        String currentDesc = null;
+        int lineNum = 0;
+        List<Item> newItemList = new ArrayList<Item>();
+        ///creating variables to aid with the scanning process
         try {
-            File menuFile = new File("Menu.txt");
-            Scanner scanMenu = new Scanner(menuFile);
-            fr = new FileReader(menuFile);
+            Scanner scanMenu = new Scanner(fileName);
+            fr = new FileReader(fileName);
             ///creating a File object and a Scanner object
-            String currentCate = null;
-            String currentName = null;
-            Float currentCost = null;
-            String currentDesc = null;
-            Item newItem;
-            List<Item> newItemList = new ArrayList<Item>();
-            ///creating variables to aid with the scanning process
             do{
                 String line = scanMenu.nextLine();
-                String[] parts = line.split(":");
-                String key = parts[0].trim();
-                String value = parts[1].trim();
-                ///creating variables used to store and define the current Line
-                switch(key) {
-                    case "Category":
-                        currentCate = value;
-                    case "Name":
-                        currentName = value;
-                    case "Cost":
-                        currentCost = Float.parseFloat(value);
-                    case "Description":
-                        currentDesc = value;
-                    default:
-                        break;
-                }
-                ///depending on what prefix a line has, the value will be saved as the current
-                /// Category, Name, Cost, or Description. If no prefix is found, no value is added.
-                if(currentName != null && currentCost != null && currentDesc != null) {
-                    newItem = new Item(currentName,currentCost,currentDesc,currentCate);
-                    newItemList.add(newItem);
-                    currentName = null;
-                    currentCost = null;
-                    currentDesc = null;
-                    ///once all the variables are set to a value, an Item is created using them,
-                    ///added to newItemList, and the Name/Cost/Description are reset to null.
-                }
-                if(line.equals("-"))
+                if(currentCate == null)
                 {
-                    menuList.add(newItemList);
-                    ///if the line is "-", which in the .txt denotes the end of a category,
-                    /// the current newItemList is added to menuList.
+                    currentCate = line;
                 }
+                else {
+                    switch(lineNum % 4)
+                    {
+                        case 1:
+                            currentName = line;
+                        case 2:
+                            currentCost = Float.parseFloat(line);
+                        case 3:
+                            currentDesc = line;
+                        case 0:
+                            newItemList.add(new Item(currentName,currentCost,currentDesc,currentCate));
+                    }
+                }
+                lineNum++;
             } while(scanMenu.hasNextLine());
             fr.close();
-            menuItems = menuList;
         }
         catch(IOException ioex) {
             System.out.println ("Error: " + ioex.getMessage());
         }
+        return newItemList;
     }
 }
